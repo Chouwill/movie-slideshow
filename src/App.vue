3c7578158003bbe0d2dcf142d4fc03921b55f33a
Chouwill <will92068@gmail.com>
2025-07-31 19:39:10 +0800

    feat:建立計數器

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import Movie from "../src/components/Ｍovie.vue" 
+
+import Count from '../src/components/Count.vue';
 </script>
 
 <template>
 <Movie />
+<Count />
 </template>
 
 <style scoped>
---
+++ src/components/Count.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from "vue";
+
+const counToal = ref(0);
+
+const Onadd = () => {
+  console.log("增加");
+  counToal.value++;
+};
+const reduceNum = () => {
+  console.log("減少");
+    counToal.value--;
+
+};
+</script>
+
+<template>
+  <div class="Count-box">
+    <h2>Count</h2>
+    {{ counToal }}
+    <button @click="Onadd">增加</button>
+    <button @click="reduceNum">減少</button>
+  </div>
+</template>
+
+<style lang="scss">
+.Count-box {
+  width: 100%;
+  margin: 3rem 0;
+}
+</style>
